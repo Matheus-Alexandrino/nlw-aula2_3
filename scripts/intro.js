b5cafbd6b40312572d839ab65b8a90285abbcf64
Matheus-Alexandrino <matwsalmeida@gmail.com>
2020-06-05 12:32:09 -0300

    init

---
+++ scripts/intro.js
@@ -0,0 +1,46 @@
+// const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteiro: true,
+//   correr() {
+
+//     return "run Forest"
+// }
+// }
+// document.write(pessoa.correr())
+
+//array
+
+// const colecaoDeBolinhas = ["blue","green", 1, {name:"My name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso
+
+//registrar função
+// function sayMyName (name) {
+//   document.write(name)
+// }
+
+// //executar 
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+
+//CONDICIONAIS
+
+// const notaFinal = 4
+
+// if (notaFinal <5) {
+//   document.write("Reprovado")
+// } else  {
+//   document.write ("Aprovado")
+// }
+
+// LOOP REPETIÇOES
+
+// for (i=0; i < 10; i ++) {
+//   document.write (`<p> ${i} </p>`)
+// }
+
+// COLOCAR API DE LOCaLIDADE 
+//fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(function(res) {return res.json() }).then ( function(data) {console.log(data)})
